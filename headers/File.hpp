28861491842010f373630595b15deba8b9d64319
Basil ALBADER <baalbade@student.42.fr>
2024-03-07 11:25:55 +0100

    WIP

--- headers/File.hpp
+++ headers/File.hpp
@@ -1,24 +1,18 @@
-#ifndef FILE_HPP
-#define FILE_HPP
-
+#pragma once
 #include "Server.hpp"
 
-class File
-{
-	public:
-		std::string	Name;
-		std::string Path;
-		std::string sender;
-		std::string receiver;
+class File {
+public:
+    std::string	Name;
+    std::string Path;
+    std::string sender;
+    std::string receiver;
 
-	private:
-		File();
+    File( std::string Name, std::string Path, std::string Sender, std::string Receiver );
+    File( const File &x );
+    ~File();
+    File & operator = ( const File &rhs );
 
-	public:
-		File( std::string Name, std::string Path, std::string Sender, std::string Receiver );
-		File( const File &x );
-		~File();
-		File & operator = ( const File &rhs );
+private:
+    File();
 };
-
-#endif
