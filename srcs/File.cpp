29742971f45c68ca25b7d8ec4dd99e9988cb4b0d
Basil ALBADER <baalbade@student.42.fr>
2024-03-07 10:43:50 +0100

    WIP

--- srcs/File.cpp
+++ srcs/File.cpp
@@ -1,10 +1,18 @@
 #include "../headers/File.hpp"
 
-File::File() {};
-File::File( std::string Name, std::string Path, std::string Sender, std::string Receiver ): Name(Name), Path(Path), sender(Sender), receiver(Receiver) {};
-File::File( const File &x ) { *this = x; };
-File::~File() {};
+// Constructor
+File::File() {}
 
+// Constructor operload
+File::File( std::string Name, std::string Path, std::string Sender, std::string Receiver ): Name(Name), Path(Path), sender(Sender), receiver(Receiver) {}
+
+// Copy constructor
+File::File( const File &x ) { *this = x; }
+
+// Destructor
+File::~File() {}
+
+// Assignment operator
 File & File::operator=( const File &rhs )
 {
 	if (this == &rhs)
@@ -14,4 +22,4 @@ File & File::operator=( const File &rhs )
 	this->sender = rhs.sender;
 	this->receiver = rhs.receiver;
 	return (*this);
-};
+}
