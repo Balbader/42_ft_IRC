ff54ef24a79b6d6c8dcd4f87b35682a47e4b314a
Basil ALBADER <baalbade@student.42.fr>
2024-03-07 11:00:37 +0100

    WIP

--- srcs/utils.cpp
+++ srcs/utils.cpp
@@ -1,19 +1,19 @@
 #include "../headers/Server.hpp"
 
-std::string fillIt(std::string str, size_t len)
-{
+std::string fillIt(std::string str, size_t len) {
 	if (str.length() < len)
 		str.insert(str.end(), len - str.length(), ' ');
+
 	else
 	{
 		str.erase(str.begin() + len - 1, str.end());
 		str.append(".");
 	}
-	return (str);
-};
 
-const std::string currentDateTime()
-{
+	return (str);
+}
+
+const std::string currentDateTime() {
     time_t     now = time(0);
     struct tm  tstruct;
     char       buf[80];
@@ -22,4 +22,4 @@ const std::string currentDateTime()
     strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
 
     return buf;
-};
+}
