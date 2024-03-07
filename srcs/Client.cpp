b0c4ac1fc1e66851d8794258fc6638d9ea9fe499
Basil ALBADER <baalbade@student.42.fr>
2024-03-07 10:31:45 +0100

    WIP

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,16 +1,17 @@
 #include "../headers/Client.hpp"
-#include <sstream>
 
-
+// Constructor
 Client::Client(): _clientfd(0), _Auth(false), _Registered(false), _isOperator(false), _NickName(), _UserName(), _FullName(), _Host("deez.nuts"), _ID(), _remotaddr(), _addrlen(), _modes(), _joinedChannels() {};
 Client::Client( int fd ): _clientfd(fd), _Auth(false), _Registered(false), _isOperator(false), _NickName(), _UserName(), _FullName(), _Host("deez.nuts"), _ID(), _remotaddr(), _addrlen(), _modes(), _joinedChannels() {};
+
+// Copy Constructor
 Client::Client( const Client& x ): _Host(x._Host) { *this = x; };
 
-
-Client & Client::operator=( const Client& rhs )
-{
+// Assignment operator
+Client & Client::operator=( const Client& rhs ) {
 	if (this == &rhs)
 		return (*this);
+
 	this->_clientfd = rhs._clientfd;
 	this->_Registered = rhs._Registered;
 	this->_isOperator = rhs._isOperator;
@@ -23,22 +24,24 @@ Client & Client::operator=( const Client& rhs )
 	this->_remotaddr = rhs._remotaddr;
 	this->_addrlen = rhs._addrlen;
 	this->_joinedChannels.insert(rhs._joinedChannels.begin(), rhs._joinedChannels.end());
-	return (*this);
-};
 
+	return (*this);
+}
+
+// Destructor
 Client::~Client() {};
 
-std::string	Client::getUserName()		const { return (this->_UserName); };
-std::string	Client::getNickName()		const { return (this->_NickName); };
-std::string	Client::getFullName()		const { return (this->_FullName); };
-std::string Client::getHost()			const { return (this->_Host); };
-std::string Client::getID()				const { return (this->_ID); }
-bool		Client::getAuth()			const { return (this->_Auth); };
-int			Client::getClientfd()		const { return (this->_clientfd); };
-int			Client::getRegistered()		const { return (this->_Registered); };
-int			Client::getisOperator()		const { return (this->_isOperator); };
-int			Client::getMode(char mode)	const
-{
+// Getters
+std::string	Client::getUserName() const { return (this->_UserName); };
+std::string	Client::getNickName() const { return (this->_NickName); };
+std::string	Client::getFullName() const { return (this->_FullName); };
+std::string Client::getHost() const { return (this->_Host); };
+std::string Client::getID()	const { return (this->_ID); }
+bool Client::getAuth() const { return (this->_Auth); };
+int	Client::getClientfd() const { return (this->_clientfd); };
+int	Client::getRegistered() const { return (this->_Registered); };
+int	Client::getisOperator() const { return (this->_isOperator); };
+int	Client::getMode(char mode) const {
 	if (mode == 'a')
 		return this->_modes.away;
 	else if (mode == 'i')
@@ -56,22 +59,21 @@ int			Client::getMode(char mode)	const
 	return (0);
 }
 
-
-void		Client::setUserName(std::string UserName)	{ this->_UserName = UserName; };
-void		Client::setNickName( std::string NickName )	{ this->_NickName = NickName; };
-void		Client::setFullName( std::string FullName )	{ this->_FullName = FullName; };
-void		Client::setID( std::string ID )				{ this->_ID = ID; };
-void		Client::setClientfd( int Clientfd )			{ this->_clientfd = Clientfd; };
-void		Client::setRegistered( int Registered )		{ this->_Registered = Registered; };
-void		Client::setAuth( int Auth )					{ this->_Auth = Auth; };
-void		Client::setIsOperator(int isOperator)
-{
+// Setters
+void Client::setUserName(std::string UserName) { this->_UserName = UserName; };
+void Client::setNickName( std::string NickName ) { this->_NickName = NickName; };
+void Client::setFullName( std::string FullName ) { this->_FullName = FullName; };
+void Client::setID( std::string ID ) { this->_ID = ID; };
+void Client::setClientfd( int Clientfd ) { this->_clientfd = Clientfd; };
+void Client::setRegistered( int Registered ) { this->_Registered = Registered; };
+void Client::setAuth( int Auth ) { this->_Auth = Auth; };
+void Client::setIsOperator(int isOperator) {
 	this->_isOperator = isOperator;
 	this->_modes.op = isOperator;
 	this->_modes.localOp = isOperator;
-};
-void		Client::setMode(int value, char mode)
-{
+}
+
+void Client::setMode(int value, char mode) {
 	if (mode == 'i')
 		this->_modes.invisible = value;
 	else if (mode == 'w')
@@ -84,63 +86,64 @@ void		Client::setMode(int value, char mode)
 		this->_modes.localOp = value;
 	else if (mode == 's')
 		this->_modes.server = value;
-};
+}
 
-int		Client::isJoined( std::string ChannelName ) const
-{
+// Methods
+int Client::isJoined( std::string ChannelName ) const {
 	if (this->_joinedChannels.find(ChannelName) != this->_joinedChannels.end())
 		return (1);
-	return (0);
-};
 
-void	Client::joinChannel( std::string ChannelName, Channel *channel )
-{
+	return (0);
+}
+
+void Client::joinChannel( std::string ChannelName, Channel *channel ) {
 	if (!isJoined(ChannelName))
 		this->_joinedChannels.insert(std::pair<std::string, Channel *>(ChannelName, channel));
-};
+}
 
-std::string	Client::JoinedChannels() const
-{
+std::string	Client::JoinedChannels() const {
 	std::string	channels;
 	std::map<std::string, Channel *>::const_iterator it = this->_joinedChannels.begin();
-	while (it != this->_joinedChannels.end())
-	{
+
+	while (it != this->_joinedChannels.end()) {
 		channels.append(BLUE + it->first + RESET + ":\n");
 		channels.append(YELLOW "\tChannel Name: " RESET + it->first + "\n");
 		channels.append(YELLOW "\tChannel Name inside class: " RESET + it->second->getName() + "\n");
 		channels.append(YELLOW  "\tChannel Creator: " RESET + it->second->getCreator()->getFullName() + "\n");
 		it++;
-	};
+	}
+
 	return (channels);
-};
+}
 
-void	Client::leaveChannel( std::string ChannelName )
-{
+void Client::leaveChannel( std::string ChannelName ) {
 	this->_joinedChannels.erase(ChannelName);
-};
+}
 
-std::string	Client::leaveAllChannels()
-{
+std::string	Client::leaveAllChannels() {
 	std::map<std::string, Channel *>::iterator it = this->_joinedChannels.begin();
-	while( it != this->_joinedChannels.end())
-	{
+
+	while( it != this->_joinedChannels.end()) {
 		std::pair<Client *, int> user(it->second->findUserRole(this->_clientfd));
+
 		if (user.second == 0)
 			it->second->removeMember(this->_clientfd);
 		else if (user.second == 1)
 			it->second->removeOperator(this->_clientfd);
 		else
 			it->second->removeVoice(this->_clientfd);
+
 		user.first->leaveChannel(it->second->getName());
 		it = this->_joinedChannels.begin();
 	}
-	return ("");
-};
 
-std::string	Client::getUserInfo() const
-{
+	return ("");
+}
+
+std::string	Client::getUserInfo() const {
 	std::string	userInfo;
     std::stringstream ss;
+
     ss << this->_joinedChannels.size();
     std::string tmp = ss.str();
 
@@ -150,11 +153,11 @@ std::string	Client::getUserInfo() const
 	userInfo.append("Host: " + this->_Host + "\n");
 	userInfo.append("Joined Channels: " + tmp + "\n");
 	userInfo.append("\n");
-	return (userInfo);
-};
 
-std::string	Client::getAllChannels() const
-{
+	return (userInfo);
+}
+
+std::string	Client::getAllChannels() const {
 	std::string channels(YELLOW "███████████████████████████████████████████████████████████████████████████████████████\n");
 	channels.append("█              █              █                    █                                  █\n");
 	channels.append("█" RESET " Channel Name " YELLOW "█ " RESET "Online Users " YELLOW "█ " RESET "Creator Of Channel " YELLOW "█ " RESET "          Channel Topic          " YELLOW "█\n");
@@ -166,8 +169,7 @@ std::string	Client::getAllChannels() const
     ss << it->second->getOnlineUsers();
     std::string tmp = ss.str();
 
-	while (it != this->_joinedChannels.end())
-	{
+	while (it != this->_joinedChannels.end()) {
 		channels.append("█              █              █                    █                                  █\n");
 		channels.append("█ " RESET + fillIt(it->first, 12));
 		channels.append(YELLOW " █      " RESET + fillIt(tmp, 7));
@@ -177,18 +179,19 @@ std::string	Client::getAllChannels() const
 		channels.append(YELLOW "█              █              █                    █                                  █\n");
 		channels.append("███████████████████████████████████████████████████████████████████████████████████████\n");
 		it++;
-	};
-	if (this->_joinedChannels.size() == 0)
-	{
+	}
+
+	if (this->_joinedChannels.size() == 0) {
 		channels.append("█                                                                                     █\n");
-		channels.append("█                                " RESET "YOU JOINED NO CHANNEL" YELLOW "                                █\n");
+		channels.append("█                       " RESET "YOU JOINED NO CHANNEL" YELLOW "                      █\n");
 		channels.append("█                                                                                     █\n");
 		channels.append("███████████████████████████████████████████████████████████████████████████████████████\n");
 	}
+
 	channels.append(RESET "\n\n");
 	return (channels);
-};
+}
 
-std::string		Client::getUserPerfix() const { return (":" + this->_NickName + "!" + this->_UserName + "@" + this->_Host + " "); };
+std::string	Client::getUserPerfix() const { return (":" + this->_NickName + "!" + this->_UserName + "@" + this->_Host + " "); }
 
-std::map<std::string, Channel *>	Client::getJoinedChannels() const { return (this->_joinedChannels); };
+std::map<std::string, Channel *>	Client::getJoinedChannels() const { return (this->_joinedChannels); }
