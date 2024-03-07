5bf26d96dc7f84af193e9cf1b2a8c293d92b0ba0
balbader <basilalbapro@gmail.com>
2024-03-07 17:22:02 +0100

    WIP

--- main.cpp
+++ main.cpp
@@ -2,18 +2,16 @@
 
 int main(int ac, char **av)
 {
-	if (ac != 3)
-	{
+	if (ac != 3) {
 		std::cout << "Usage ./ircserv <port> <password>" << std::endl;
 		exit(-1);
 	}
-	Server	srv("DEEZ.NUTS", 10, av[1], av[2]);
-	try
-	{
+
+	Server srv("DEEZ.NUTS", 10, av[1], av[2]);
+
+	try {
 		srv.startServer();
-	}
-	catch (const std::exception& e)
-	{
+	} catch (const std::exception& e) {
 		std::cerr << e.what() << '\n';
 	}
 };
