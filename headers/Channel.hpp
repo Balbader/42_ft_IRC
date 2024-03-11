9941cfbe0099e92ebe91097ec8299b915e5f72cc
Basil ALBADER <baalbade@student.42.fr>
2024-03-11 10:58:13 +0100

    WIP

--- headers/Channel.hpp
+++ headers/Channel.hpp
@@ -15,57 +15,57 @@
 #include "Server.hpp"
 
 class Channel {
-	private:
-		char _prefix;
-		Client* _creator;
-		int _onlineUsers;
-		std::string	_name;
-		std::string	_key;
-		std::string	_topic;
-		std::map<int, Client *>	_members;
-		std::map<int, Client *>	_operators;
-		std::map<int, Client *>	_voice;
-		std::vector<std::string> _banned;
+private:
+    char _prefix;
+    Client* _creator;
+    int _onlineUsers;
+    std::string	_name;
+    std::string	_key;
+    std::string	_topic;
+    std::map<int, Client *>	_members;
+    std::map<int, Client *>	_operators;
+    std::map<int, Client *>	_voice;
+    std::vector<std::string> _banned;
 
-		Channel();
+    Channel();
 
-	public:
-		Channel( std::string channelName, Client *Creater );
-		Channel( std::string channelName, std::string channelKey, Client *Creater );
-		Channel( const Channel& x );
-		Channel &operator=( const Channel& rhs );
-		~Channel();
+public:
+    Channel( std::string channelName, Client *Creater );
+    Channel( std::string channelName, std::string channelKey, Client *Creater );
+    Channel( const Channel& x );
+    Channel &operator=( const Channel& rhs );
+    ~Channel();
 
-        // Getters
-		char const &getPrefix()	const;
-		int const &getOnlineUsers()	const;
-		std::string	const &getName() const;
-		std::string	const &getKey() const;
-		std::string	const &getTopic() const;
-		std::map<int, Client *> const &getMembers() const;
-		std::map<int, Client *> const &getOperators() const;
-		std::map<int, Client *> const &getVoice() const;
-		std::map<std::string, Client *>	const &getBanned() const;
+    // Getters
+    char const &getPrefix()	const;
+    int const &getOnlineUsers()	const;
+    std::string	const &getName() const;
+    std::string	const &getKey() const;
+    std::string	const &getTopic() const;
+    std::map<int, Client *> const &getMembers() const;
+    std::map<int, Client *> const &getOperators() const;
+    std::map<int, Client *> const &getVoice() const;
+    std::map<std::string, Client *>	const &getBanned() const;
 
-		Client*	getCreator() const;
-		std::map<int, Client *>	getAllUsers() const;
-		std::pair<Client *, int> findUserRole( int i );
-		std::string	listAllUsers() const;
+    Client*	getCreator() const;
+    std::map<int, Client *>	getAllUsers() const;
+    std::pair<Client *, int> findUserRole( int i );
+    std::string	listAllUsers() const;
 
-        // Methods
-		int	addMember( Client *member );
-		int	addOperator( Client *member );
-		void addVoice( Client *member );
-		int	banUser( Client *member );
-		void removeOperator( int i );
-		void removeVoice( int i );
-		void removeBanned( std::string NickName );
-		void removeMember( int i );
+    // Methods
+    int	addMember( Client *member );
+    int	addOperator( Client *member );
+    void addVoice( Client *member );
+    int	banUser( Client *member );
+    void removeOperator( int i );
+    void removeVoice( int i );
+    void removeBanned( std::string NickName );
+    void removeMember( int i );
 
-        // Setters
-		void setPrefix(char prefix);
-		void setOnlineUsers(int online);
-		void setName(std::string name);
-		void setKey(std::string key);
-		void setTopic(std::string topic);
+    // Setters
+    void setPrefix(char prefix);
+    void setOnlineUsers(int online);
+    void setName(std::string name);
+    void setKey(std::string key);
+    void setTopic(std::string topic);
 };
