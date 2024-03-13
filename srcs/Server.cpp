7ef428b40f528840162d4737c70bfb88630f8438
balbader <basilalbapro@gmail.com>
2024-03-13 14:14:13 +0100

    WIP

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -42,42 +42,63 @@ std::string	Server::_printMessage(std::string num, std::string nickname, std::st
 
 void sendClient(int fd_client, std::string msg) {
 
-    std::cout << "[server]: " << fd_client << " send this request :" << msg << "\n" << std::endl;
+    std::cout << "[server]: " << fd_client << " sent this request :" << msg << "\n" << std::endl;
 
     if (send(fd_client, msg.c_str(), msg.length(), 0) == -1)
         std::cout << "send() error: " << strerror(errno) << std::endl;
 }
 
+// handle new client connection
+// accepts the connection
+// updates the poll array
+// sends a welcome message
+// logs the event
 void Server::_newClient(void) {
-	struct sockaddr_storage	remotaddr;
-	socklen_t addrlen;
-	int newfd;
+    struct sockaddr_storage remotaddr;
+    socklen_t addrlen = sizeof(remotaddr);
 
-	addrlen = sizeof remotaddr;
-	newfd = accept(this->_socketfd, (struct sockaddr*)&remotaddr, &addrlen);
+	// accept new connection on the server's listening socket '_socketfd'
+	// stores the client's address in 'remotaddr'
+	// stores the file descriptor for the new connection in newfd
+    int newfd = accept(this->_socketfd, (struct sockaddr*)&remotaddr, &addrlen);
 
-	if (newfd == -1)
-		std::cout << "accept() error: " << strerror(errno) << std::endl;
+	// if it fails
+    if (newfd == -1) {
+        std::cerr << "accept() error: " << strerror(errno) << std::endl;
+    }
+	
+	// if success
 	else {
-		_addToPoll(newfd);
+        _addToPoll(newfd); // Add the new client to the poll array
 
-		std::string welcome = _welcomemsg();
+        std::string welcome = _welcomemsg();
+        if (!welcome.empty()) {
+            sendClient(newfd, welcome); // Send a welcome message to the client
+        }
 
-        sendClient(newfd, welcome);
-
-		std::cout << "[" << currentDateTime() << "]: new connection from " << inet_ntoa(((struct sockaddr_in*)&remotaddr)->sin_addr) << " on socket " << newfd << std::endl;
-	}
+        // Logging the new connection
+        char ipstr[INET6_ADDRSTRLEN];
+        void *addr = &((struct sockaddr_in*)&remotaddr)->sin_addr;
+        inet_ntop(remotaddr.ss_family, addr, ipstr, sizeof(ipstr));
+        std::cout << "[" << currentDateTime() << "]: New connection from " << ipstr << " on socket " << newfd << std::endl;
+    }
 }
 
+
+// main loop responsible for handling incoming connections and client requests
 void Server::startServer(void) {
+	
+	// infinite loop continuously checking for new events
 	while (77) {
 		int poll_count = poll(this->_pfds, this->_online_c, -1);
 
+		// check error
 		if (poll_count == -1) {
 			std::cout << "poll() error: " << strerror(errno) << std::endl;
 			exit(-1);
 		}
 
+		// iterates through the array of 'pollfd' structures
 		for (int i = 0; i < this->_online_c; i++) {
 			if (this->_pfds[i].revents & POLLIN) {
 				if (this->_pfds[i].fd == this->_socketfd)
