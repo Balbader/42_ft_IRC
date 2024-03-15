b766f835a928ab3d594fbc40af33be90c79d533b
balbader <basilalbapro@gmail.com>
2024-03-15 14:17:34 +0100

    WIP

--- headers/Server.hpp
+++ headers/Server.hpp
@@ -75,12 +75,12 @@ private:
 	std::string _printHelpInfo();
 	std::string	_printUserModes(std::string ret, int i);
 	std::vector<std::string> _commaSeparator(std::string arg);
-	int	_createPrvChannel( std::string ChannelName, std::string ChannelKey, int	 CreatorFd );
-	int	_createChannel( std::string channelName, int CreatorFd );
-	int	_partChannel( std::string ChannelName, int i, std::string message, int isPart );
-	std::string	_DeezNuts( Request request, int i );
-	std::string	_sendFile( Request request, int i );
-	std::string	_getFile( Request request, int i );
+	int	_createPrvChannel(std::string ChannelName, std::string ChannelKey, int	 CreatorFd);
+	int	_createChannel(std::string channelName, int CreatorFd);
+	int	_partChannel(std::string ChannelName, int i, std::string message, int isPart);
+	std::string	_DeezNuts(Request request, int i);
+	std::string	_sendFile(Request request, int i);
+	std::string	_getFile(Request request, int i);
 	std::string	_getBotMessage();
 	std::string	_listAllChannels() const;
 	std::string	_channelInfo(std::string ChannelName, int i);
@@ -91,14 +91,14 @@ private:
 	std::string	_notice(Request request, int i);
 	std::string	_privToUser(std::string User, std::string message, std::string cmd, int i);
 	std::string	_privToChannel(std::string ChannelName, std::string message, int i);
-	std::string	_sendToAllUsers( Channel *channel, int senderFd, std::string message);
+	std::string	_sendToAllUsers(Channel *channel, int senderFd, std::string message);
 	std::string	_getPassword() const;
 
 public:
 	Server(std::string Name, int max_online, std::string Port, std::string Password);
 	Server(const Server & x);
 	~Server();
-	Server & operator=( const Server & rhs );
+	Server & operator=(const Server & rhs);
 	void	startServer(void);
 };
 
