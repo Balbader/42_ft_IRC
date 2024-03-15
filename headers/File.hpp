42e43aceb20d5305bd4606c25df50fc06e03d4ce
balbader <basilalbapro@gmail.com>
2024-03-15 14:17:33 +0100

    WIP

--- headers/File.hpp
+++ headers/File.hpp
@@ -8,10 +8,10 @@ public:
     std::string sender;
     std::string receiver;
 
-    File( std::string Name, std::string Path, std::string Sender, std::string Receiver );
-    File( const File &x );
+    File(std::string Name, std::string Path, std::string Sender, std::string Receiver);
+    File(const File & x);
     ~File();
-    File & operator = ( const File &rhs );
+    File & operator=(const File & rhs);
 
 private:
     File();
