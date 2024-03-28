083319060daf94a13a9647d978fee4488a492467
Basil ALBADER <baalbade@student.42.fr>
2024-03-28 14:53:03 +0100

    WIP

--- srcs/split_util.cpp
+++ srcs/split_util.cpp
@@ -18,14 +18,3 @@ std::vector<std::string> split(const std::string &src, const std::string &sep) {
   split_str.push_back(str);
   return (split_str);
 }
-
-int main() {
-  std::string str = "PASS coucou\r\n CONNECT refvdc\r\nlala";
-  std::string sep = "\r\n";
-
-  std::vector<std::string> res = split(str, sep);
-  for (std::vector<std::string>::iterator it = res.begin() ; it != res.end() ; it++)
-    std::cout << "[]" << *it << std::endl;
-
-  return (0);
-}
