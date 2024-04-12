519a1cd3cd1556bcff706da4ae0929615ac43fb4
Basil ALBADER <baalbade@student.42.fr>
2024-04-12 18:09:54 +0200

    WIP

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -8,13 +8,18 @@ int main(int ac, char **av) {
     int port;
 
     try {
+        // argument check
         if (ac != 3)
 			throw std::invalid_argument("Invalid number of arguments");
+
+        // parse command line arguments
         port = std::atoi(av[1]);
         password = av[2];
+
+        // server initialization and start
         Server server(password, port);
         server.startServer();
-    } catch (const std::exception& e) {
+    } catch (const std::exception& e) { // exception handling
         Logger().error("main", e.what());
         return EXIT_FAILURE; 
     } 
