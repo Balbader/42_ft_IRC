03e9e7e703c34139cda151d340074416ca888616
Basil ALBADER <baalbade@student.42.fr>
2024-04-12 17:24:00 +0200

    WIP

--- srcs/CommandExecutor.cpp
+++ srcs/CommandExecutor.cpp
@@ -1,21 +1,33 @@
 #include <Server.hpp>
 
 void Server::executeCommands(Client &client, std::vector<Command> &commands) {
+    // logging the start of command execution
 	LOGGER.info("executeCommands", "Executing commands");
+
+    // initialize nickname for logging
 	std::string nick = "(unidentified)";
+
+    // interate over commands
 	std::vector<Command>::iterator it = commands.begin();
 	for (; it < commands.end(); it++) {
+        // update nickname if identified
 		if (client.getNickname().size() > 0) {
 			nick = client.getNickname();
 		}
+        
+        // log each command
 		std::ostringstream logMessage;
 		logMessage << "Client " << nick << " on fd " << client.getFd() << " sent: " << (*it).cmd ;
 		LOGGER.info("executeCommands", logMessage.str());
+
+        // execute individual command
 		executeCommand(client, (*it));
 	}
 }
 
 void Server::executeCommand(Client &client, Command &command) {
+
+    // command dispatch
 	if (command.cmd == "NICK") {
 		nick(client, command);
 	} else if (command.cmd == "PASS") {
@@ -52,6 +64,9 @@ void Server::executeCommand(Client &client, Command &command) {
 		client.setSendData(unknowncommand(client, command.cmd));
 	}
 
+    // post command processing
+    // after a command is executed, the function checks if the client has completed all necessary registration steps
+    // (PASS, USER< NICK) and has not yet received a welcome message
 	if (client.getRegistration() == (PASS_FLAG | USER_FLAG | NICK_FLAG)
 		&& !client.getWelcome()) {
 		client.setWelcome(true);
