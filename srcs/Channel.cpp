0d5b0eb47f74cecca516deb0f4b0a5646f5fc788
Basil ALBADER <baalbade@student.42.fr>
2024-04-12 18:18:51 +0200

    WIP

--- srcs/Channel.cpp
+++ srcs/Channel.cpp
@@ -18,15 +18,15 @@ Channel::~Channel(void) {
 
 
 // getters
-std::string Channel::getName() const { return name; };
+std::string Channel::getName() const { return name; }
 
 std::string Channel::getTopic() const { return topic; }
 
-Client &Channel::getCreator() { return *creator; };
+Client &Channel::getCreator() { return *creator; }
 
 std::set<char> &Channel::getMode() { return modes; }
 
-unsigned int Channel::getUserLimit() const { return userLimit; };
+unsigned int Channel::getUserLimit() const { return userLimit; }
 
 std::map<Client *, unsigned int> &Channel::getClients() { return clients; }
 
@@ -46,7 +46,7 @@ std::string Channel::getStrModes() {
 		}
 	}
 	return (modeStr);
-};
+}
 
 std::map<Client *, unsigned int>::iterator Channel::getClientByNick(std::string clientNickname) {
 	LOGGER.info("getClientByNick", "Looking for client " + clientNickname);
@@ -56,13 +56,13 @@ std::map<Client *, unsigned int>::iterator Channel::getClientByNick(std::string 
 		it++;
 	}
 	return (it);
-};
+}
 
 
 // setters
-void Channel::setTopic(std::string topic) { this->topic = topic; };
+void Channel::setTopic(std::string topic) { this->topic = topic; }
 void Channel::setPassword(std::string password) { this->password = password; }
-void Channel::setUserLimit(unsigned int limit) { userLimit = limit; };
+void Channel::setUserLimit(unsigned int limit) { userLimit = limit; }
 
 bool Channel::setOperator(std::string clientNickname, bool newValue) {
 	std::ostringstream logMessage;
@@ -79,7 +79,7 @@ bool Channel::setOperator(std::string clientNickname, bool newValue) {
 		return true;
 	}
 	return (false);
-};
+}
 
 void Channel::setSpeaker(std::string clientNickname, bool newValue) {
 	std::ostringstream logMessage;
@@ -94,7 +94,7 @@ void Channel::setSpeaker(std::string clientNickname, bool newValue) {
 		else
 			it->second &= (~USER_SPEAKER);
 	}
-};
+}
 
 
 // methods
@@ -116,29 +116,50 @@ void Channel::removeClient(Client &client) {
 	}
 }
 
+
+/*
+ * The broadcast method in the Channel class is designed to send a message to all clients connected
+ * to a specific channel, with an option to include or exclude the sender of the message.
+*/
 void Channel::broadcast(Client &sender, std::string message, bool toSend) {
 	std::map<Client *, unsigned int>::iterator it = clients.begin();
 
+    // logging the Broadcasting action
 	LOGGER.info("broadcast", "Broadcasting message to all clients in channel " + name);
+
+    // itterate and send message
 	for (; it != clients.end(); it++) {
+        // sender included
 		if (it->first == &sender && toSend) {
 			(*it->first).setSendData(message);
-		} else if (it->first != &sender) {
+		} else if (it->first != &sender) { // sender excluded
 			(*it->first).setSendData(message);
 		}
 	}
 }
 
+
+/*
+ * The toggleMode function in the Channel class is used to manage the modes of a channel,
+ * which can control various behaviors and settings within the channel.
+ * The function toggles the presence of a specified mode character in a set that represents
+ * the current modes active in the channel. 
+*/
 bool Channel::toggleMode(char mode, bool on) {
 	std::size_t before = modes.size();
 	std::ostringstream logMessage;
 
+    // logging the action
 	logMessage << "Toggling mode " << mode << " to " <<on;
 	LOGGER.info("toggleMode", logMessage.str());
+
+    // toggle the mode
 	if (on)
 		modes.insert(mode);
 	else
 		modes.erase(mode);
+
+    // return change status
 	return !(before == modes.size());
 }
 
@@ -168,18 +189,27 @@ bool Channel::isInvited(std::string nickname) {
 		return true;
 	}
 	return false;
-};
+}
 
 void Channel::addInvite(std::string nickname) {
 	invited.insert(toIrcUpperCase(nickname));
-};
+}
 
 void Channel::removeInvited(std::string nickname) {
 	invited.erase(toIrcUpperCase(nickname));
-};
+}
 
+
+/*
+ * The asureOperator method in the Channel class is designed to ensure that there is at least
+ * one operator in the channel. If no operators are currently assigned, it assigns the role of operator
+ * to the first client in the channel's client list. 
+*/
 void Channel::asureOperator() {
+    // logging initail check
 	LOGGER.info("asureOperator", "Checking the need for a new oper");
+
+    // check for existing operators
 	std::map<Client *, unsigned int>::iterator it;
 	it = clients.begin();
 	while (it != clients.end()) {
@@ -187,10 +217,14 @@ void Channel::asureOperator() {
 			return;
 		it++;
 	}
+
+    // assign new operator
 	it = clients.begin();
 	if (it != clients.end()) {
 		this->setOperator(it->first->getNickname(), true);
 		LOGGER.info("asureOperator", "Setting " + it->first->getNickname() + " as a new operator");
+
+        // log and broadcast new operator status
 		std::stringstream ss;
 		ss << ":" << it->first->getNickname();
 		ss << " MODE";
@@ -199,4 +233,4 @@ void Channel::asureOperator() {
 		ss << "\r\n";
 		this->broadcast(*it->first, ss.str(), true);
 	}
-};
+}
